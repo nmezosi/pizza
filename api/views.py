c75b6e468da17393f47f0e77ffa56939070784e0
Norbert Mezősi <nmezosi@gmail.com>
2023-09-21 17:07:21 +0200

    elso commit

---
+++ api/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse,JsonResponse
+from .models import Pizza
+# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hello világ!</h1>")
+
+def getPizzas(request):
+
+    pizzas = Pizza.objects.all()
+    li = []
+    for pizza in pizzas:
+        li.append(pizza.serializer())
+    
+    return JsonResponse({"data":li})
